8fe50df779c93c6d1766e12759bdf2309fb7efb1
Daniele Locatelli <daniele3.locatelli@mail.polimi.it>
2023-04-22 17:27:20 +0200

    Allow epsilon comparison

--- demo.m
+++ demo.m
@@ -5,7 +5,7 @@ addpath(genpath('.'));
 % Loading data: X contains data points, whereas G is the ground truth
 % segmentation
 
-showGraphs = true;
+showGraphs = false;
 
 clusterSize = 50;
 clusterThreshold = 0;
@@ -18,58 +18,68 @@ labelled_data = false;
         G = generateGTLbls(nClusters, clusterSize, nOutliers); %#ok<UNRCH>
 end
 
-
-epsilon = 0.085; % An inlier threshold value  epsilon has to be specified.
-% If the ground truth labels are provided: true
-% Otherwise: false
+epsilonRange= linspace(0.02, 0.25, 30);
+%epsilonRange = 0.085; % An inlier threshold value  epsilon has to be specified.
 
 
-%% Perform T-Linkage
-[lblsTLinkage, T] = t_linkage(X, distFun, epsilon, cardmss, hpFun);
+outlierRange = 0:0.05:1; % TODO to use
 
-%% Perform Dynamic T-Linkage
-lblsDynTLinkage = dynamicTLinkage(X, T, G, lblsTLinkage, epsilon, isMergeableGricModel, clusterThreshold);
+for i=1:length(epsilonRange)
+    if length(epsilonRange) > 1
+        display("Inlier threshold variation " + i + " out of " + length(epsilonRange))
+    end
+    
+    epsilon = epsilonRange(i);
+    
+    %% Perform T-Linkage
+    [lblsTLinkage, T] = t_linkage(X, distFun, epsilon, cardmss, hpFun);
+    
+    %% Perform Dynamic T-Linkage
+    lblsDynTLinkage = dynamicTLinkage(X, T, G, lblsTLinkage, epsilon, isMergeableGricModel, clusterThreshold);
+    
+    %% Outlier rejection step
+    
+    %T-Linkage fit a model to all the data points. Outlier can be found in
+    %different ways (T-Linkage is agonostic about the outlier rejection strategy),
+    %for example discarding too small cluster, or exploiting the randomness of
+    %a model.
+    
+    lblsTLinkage = operateOnOutliers(lblsTLinkage, cardmss);
+    lblsDynTLinkage = operateOnOutliers(lblsDynTLinkage, cardmss);
+    
+    
+    %% Order labels step
+    
+    lblsTLinkage = orderClusterLabels(lblsTLinkage, clusterSize);
+    lblsDynTLinkage = orderClusterLabels(lblsDynTLinkage, clusterSize);
+    
+    %% Showing results
+    if showGraphs
+        figure('name','Assigned labels')
+        subplot(1,3,1); gscatter(X(1,:),X(2,:), G); axis equal; title('GroundTruth'); legend off
+        subplot(1,3,2); gscatter(X(1,:),X(2,:), lblsTLinkage); axis equal; title('T linkage'); legend off
+        subplot(1,3,3); gscatter(X(1,:),X(2,:), lblsDynTLinkage); axis equal; title('Dyn T linkage'); legend off
+    end
+    
+    %% Compare clustering
+    
+    tLinkageMetrics = compareClustering(G, lblsTLinkage);
+    dynTLinkageMetrics = compareClustering(G, lblsDynTLinkage);
 
-%% Outlier rejection step
+    metrics(i).tLinkage = tLinkageMetrics;
+    metrics(i).dynTLinkage = dynTLinkageMetrics;
 
-%T-Linkage fit a model to all the data points. Outlier can be found in
-%different ways (T-Linkage is agonostic about the outlier rejection strategy),
-%for example discarding too small cluster, or exploiting the randomness of
-%a model.
-
-lblsTLinkage = operateOnOutliers(lblsTLinkage, cardmss);
-lblsDynTLinkage = operateOnOutliers(lblsDynTLinkage, cardmss);
-
-
-%% Order labels step
-
-lblsTLinkage = orderClusterLabels(lblsTLinkage, clusterSize);
-lblsDynTLinkage = orderClusterLabels(lblsDynTLinkage, clusterSize);
-
-%% Showing results
-if showGraphs
-    figure('name','Assigned labels')
-    subplot(1,3,1); gscatter(X(1,:),X(2,:), G); axis equal; title('GroundTruth'); legend off
-    subplot(1,3,2); gscatter(X(1,:),X(2,:), lblsTLinkage); axis equal; title('T linkage'); legend off
-    subplot(1,3,3); gscatter(X(1,:),X(2,:), lblsDynTLinkage); axis equal; title('Dyn T linkage'); legend off
 end
 
-%% Compare clustering
-
-tLinkageMetrics = compareClustering(G, lblsTLinkage);
-dynTLinkageMetrics = compareClustering(G, lblsDynTLinkage);
-
-%% INLIER and OUTLIER RANGE
-inlierRange = 0.005:0.0075:0.2;
-outlierRange = 0:0.05:1;
-%% INLIER THRESHOLD COMPARISON
-%load("./DendrogramUtils/Scores&Params_InlierThresholdComparison.mat")
-if showGraphs
+%% INLIER ARI COMPARISON
+if length(epsilonRange) > 1
+    finalTLinkageMetric = [metrics.tLinkage];
+    finalDynTLinkageMetric = [metrics.dynTLinkage];
     figure('name','Ari')
-    plot(inlierRange, tLinkageMetrics.ariScore, "-", "LineWidth", 2, ...
+    plot(epsilonRange, [finalTLinkageMetric.misclassErr], "-", "LineWidth", 2, ...
         "Marker", "o")
     hold on
-    plot(inlierRange, dynTLinkageMetrics.ariScore, "-", "LineWidth", 2, ...
+    plot(epsilonRange, [finalDynTLinkageMetric.misclassErr], "-", "LineWidth", 2, ...
         "Marker", "+")
     lgd = legend("T-Linkage ARI", "Dynamic T-Linkage ARI");
     lgd.FontSize = 15; % Change the font size to 14 points
@@ -79,24 +89,7 @@ if showGraphs
     ylim([0 0.8])
 end
 
-%% INLIER THRESHOLD COMPARISON - ARI & NMI
-%{
-if showGraphs
-    figure
-    %load("./DendrogramUtils/Scores&Params_InlierThresholdComparison.mat")
-    v = 0.005:0.0075:0.2;
-    plot(v, tLinkageMetrics.ariScore, "o--", "LineWidth", 2, "Color", "#D95319")
-    hold on
-    plot(v, dynTLinkageMetrics.ariScore, "s-", "LineWidth", 2, "Color", "#0072BD")
-    % plot(v, smoothdata(nmiScore(:, 1)), "--", "LineWidth", 2, "Color", "#0072BD")
-    % plot(v, smoothdata(nmiScore(:, 2)), "-", "LineWidth", 2, "Color", "#0072BD")
-    lgd = legend("T-Linkage ARI \times NMI", "LOF Dynamic T-Linkage ARI \times NMI");
-    lgd.FontSize = 15; % Change the font size to 14 points
-    title("Comparison T-Linkage vs. LOF Dynamic T-Linkage")
-    xlabel("\epsilon", "FontSize", 16)
-    ylabel("ARI \times NMI", "FontSize", 14)
-end
-%}
+
 %% Reference
 % When using the code in your research work, please cite the following paper:
 % Luca Magri, Andrea Fusiello, T-Linkage: A Continuous Relaxation of
